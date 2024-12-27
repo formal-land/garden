ec174332fdb6b7f7a80214c918705a482e413af2
Antoine James <mushow@proton.me>
2024-12-27 13:08:40 +0100

    feat: add from_quarters definition

--- Garden/01/Keccak.v
+++ Garden/01/Keccak.v
@@ -1,6 +1,5 @@
 Require Import Coq.Lists.List.
 Require Import Coq.ZArith.ZArith.
-Require Import Coq.Strings.String.
 
 Import ListNotations.
 
@@ -35,11 +34,42 @@ End Variable_.
 
 Module Keccak.
 
-  Fixpoint two_pow (n : nat) : nat :=
-  match n with
-  | 0 => 1
-  | S n' => 2 * two_pow n'
-  end.
+  Fixpoint var_two_pow (n : nat) : Variable_.t :=
+    match n with
+    | 0 => Variable_.one
+    | S n' => Variable_.mul (Variable_.add Variable_.one Variable_.one) (var_two_pow n')
+    end.
+
+  Definition nth_or_default {A : Type} (default : A) (l : list A) (n : nat) : A :=
+    nth n l default.
+
+  Definition grid_100 (quarters : list Variable_.t) (y x q : nat) : Variable_.t :=
+    nth_or_default Variable_.zero quarters (q + (Z.to_nat QUARTERS) * (x + (Z.to_nat DIM) * y)).
+
+  Definition grid_20 (quarters : list Variable_.t) (x q : nat) : Variable_.t :=
+    nth_or_default Variable_.zero quarters (q + (Z.to_nat QUARTERS) * x).
+
+  Definition from_quarters (quarters : list Variable_.t) (y : option nat) (x : nat) : option Variable_.t :=
+    match y with
+    | Some y' =>
+      if Nat.eqb (length quarters) 100 then
+        let v :=
+          Variable_.add (grid_100 quarters y' x 0) (Variable_.add
+            (Variable_.mul (var_two_pow 16) (grid_100 quarters y' x 1))
+            (Variable_.add
+            (Variable_.mul (var_two_pow 32) (grid_100 quarters y' x 2))
+            (Variable_.mul (var_two_pow 48) (grid_100 quarters y' x 3)))) in Some v
+      else None
+    | None =>
+      if Nat.eqb (length quarters) 20 then
+        let v :=
+          Variable_.add (grid_20 quarters x 0) (Variable_.add
+            (Variable_.mul (var_two_pow 16) (grid_20 quarters x 1))
+            (Variable_.add
+            (Variable_.mul (var_two_pow 32) (grid_20 quarters x 2))
+            (Variable_.mul (var_two_pow 48) (grid_20 quarters x 3)))) in Some v
+        else None
+    end.
 
   Definition constrain_theta (self : Variable_.t) (step : Steps.t) : list (list (list Variable_.t)) :=
     let state_c := repeat (repeat Variable_.zero (Z.to_nat QUARTERS)) (Z.to_nat DIM) in
