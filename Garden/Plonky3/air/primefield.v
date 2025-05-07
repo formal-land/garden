c459bc890b9f7a1b20e5823938188500c914d396
Yunkai Zhang <yunkai.zhang22@imperial.ac.uk>
2025-05-07 17:25:39 +0100

    refactor: moved all definitions to PrimeField level

--- Garden/Plonky3/air/primefield.v
+++ Garden/Plonky3/air/primefield.v
@@ -9,6 +9,8 @@ Module Type PrimeField.
   (* unused abstraction: Parameter F : Type. *)
   Parameter p : Z.  (* The prime characteristic *)
 
+  Definition mod_p (x : Z) : Z := Z.modulo x p.
+
   (* 
     later we need an assertion here that all operations are
     within the prime field with characteristic p,
@@ -16,14 +18,23 @@ Module Type PrimeField.
   *)
 
   (* field operations *)
-  Parameter zero : Z.
-  Parameter one : Z.
-  Parameter add : Z -> Z -> Z.
-  Parameter sub : Z -> Z -> Z.
-  Parameter neg : Z -> Z.
-  Parameter mul : Z -> Z -> Z.
-  Parameter inv : Z -> Z.
-  Parameter div : Z -> Z -> Z.
+  Definition zero : Z := 0.
+  Definition one : Z := 1.
+  Definition add (a b : Z) : Z := 
+    mod_p (a + b).
+  
+  Definition mul (a b : Z) : Z :=
+    mod_p (a * b).
+    
+  Definition neg (a : Z) : Z :=
+    mod_p (p - a).
+    
+  Definition sub (a b : Z) : Z :=
+    mod_p (a + p - b).
+    
+  Definition inv (a : Z) : Z := mod_p (a ^ (p - 2)).
+
+  Definition div (a b : Z) : Z := mul a (inv b).
 
   (* integer conversions *)
   (*
@@ -33,12 +44,25 @@ Module Type PrimeField.
   Parameter of_Z : Z -> F.
   Parameter to_Z : F -> Z.
   *)
-  Parameter of_nat : nat -> Z.
-  Parameter to_nat : Z -> nat.
-  Parameter of_bool : bool -> Z.
+
+  Definition of_nat (n : nat) : Z := Z.of_nat n.
+  Definition to_nat (a : Z) : nat := Z.to_nat a.
+  Definition of_bool (b : bool) : Z := if b then one else zero.
 
   (* Field properties *)
   Parameter p_prime : IsPrime p.
+
+  (* Additional operations in field.rs *)
+  Definition double (a : Z) : Z := add a a.
+  Definition square (a : Z) : Z := mul a a.
+  Definition cube (a : Z) : Z := mul (square a) a.
+
+  (* for boolean inputs only. *)
+  Definition xor (a b : Z) : Z := sub (add a b) (mul a (double b)).
+  Definition xor3 (a b c : Z) : Z := xor (xor a b) c.
+  Definition andn (a b : Z) : Z := mul (sub one a) b.
+
+
 
   (* Basic algebraic properties *)
   Axiom add_comm : forall a b, add a b = add b a.
@@ -59,10 +83,9 @@ Module Type PrimeField.
   (* Boolean to field conversion *)
   Axiom of_bool_true : of_bool true = one.
   Axiom of_bool_false : of_bool false = zero.    
-
 End PrimeField.
 
-(* An implementation: Mersenne prime p = 2^31 - 1.
+(* (* An implementation: Mersenne prime p = 2^31 - 1.
   The computations here are done primitively, especially the implementation of inverse.
   Later we could replace the implementations with mathcomp.
  *)
@@ -138,4 +161,4 @@ Module Mersenne31 <: PrimeField.
   Axiom of_bool_true : of_bool true = one.
   Axiom of_bool_false : of_bool false = zero.    
 End Mersenne31.
-
+ *)
