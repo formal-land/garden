2603356aa407e1f5de287a473a4f9f3d2a29e16f
Yunkai Zhang <yunkai.zhang22@imperial.ac.uk>
2025-05-07 17:59:11 +0100

    feat: modularised prime field creation

--- Garden/Plonky3/air/primefield.v
+++ Garden/Plonky3/air/primefield.v
@@ -4,66 +4,38 @@ Require Import Coq.ZArith.Znumtheory.
 Open Scope Z_scope.
 
 
-
 Module Type PrimeField.
-  (* unused abstraction: Parameter F : Type. *)
-  Parameter p : Z.  (* The prime characteristic *)
-
+  Parameter p : Z. (* The prime characteristic *)
   Definition mod_p (x : Z) : Z := Z.modulo x p.
-
-  (* 
-    later we need an assertion here that all operations are
-    within the prime field with characteristic p,
-    so all elements are in the range [0, p-1].
-  *)
-
+  
   (* field operations *)
   Definition zero : Z := 0.
   Definition one : Z := 1.
-  Definition add (a b : Z) : Z := 
-    mod_p (a + b).
-  
-  Definition mul (a b : Z) : Z :=
-    mod_p (a * b).
-    
-  Definition neg (a : Z) : Z :=
-    mod_p (p - a).
-    
-  Definition sub (a b : Z) : Z :=
-    mod_p (a + p - b).
-    
+  Definition add (a b : Z) : Z := mod_p (a + b).
+  Definition mul (a b : Z) : Z := mod_p (a * b).
+  Definition neg (a : Z) : Z := mod_p (p - a).
+  Definition sub (a b : Z) : Z := mod_p (a + p - b).
+
+  (* inv and div only works for non-zeroes. *)
   Definition inv (a : Z) : Z := mod_p (a ^ (p - 2)).
-
   Definition div (a b : Z) : Z := mul a (inv b).
-
+  
   (* integer conversions *)
-  (*
-  These two are used as placeholders now 
-  because we temporarily use Z as the field type instead of F as abstraction.
-  =======
-  Parameter of_Z : Z -> F.
-  Parameter to_Z : F -> Z.
-  *)
-
   Definition of_nat (n : nat) : Z := Z.of_nat n.
   Definition to_nat (a : Z) : nat := Z.to_nat a.
   Definition of_bool (b : bool) : Z := if b then one else zero.
-
+  
   (* Field properties *)
   Parameter p_prime : IsPrime p.
-
+  
   (* Additional operations in field.rs *)
   Definition double (a : Z) : Z := add a a.
   Definition square (a : Z) : Z := mul a a.
   Definition cube (a : Z) : Z := mul (square a) a.
-
-  (* for boolean inputs only. *)
   Definition xor (a b : Z) : Z := sub (add a b) (mul a (double b)).
   Definition xor3 (a b c : Z) : Z := xor (xor a b) c.
   Definition andn (a b : Z) : Z := mul (sub one a) b.
-
-
-
+  
   (* Basic algebraic properties *)
   Axiom add_comm : forall a b, add a b = add b a.
   Axiom add_assoc : forall a b c, add (add a b) c = add a (add b c).
@@ -76,89 +48,101 @@ Module Type PrimeField.
   Axiom sub_def : forall a b, sub a b = add a (neg b).
   Axiom div_def : forall a b, div a b = mul a (inv b).
   Axiom mul_inv : forall a, a <> zero -> mul a (inv a) = one.
-
-  (* Distributivity *)
   Axiom distrib : forall a b c, mul a (add b c) = add (mul a b) (mul a c).
-  
-  (* Boolean to field conversion *)
   Axiom of_bool_true : of_bool true = one.
-  Axiom of_bool_false : of_bool false = zero.    
+  Axiom of_bool_false : of_bool false = zero.
 End PrimeField.
 
-(* (* An implementation: Mersenne prime p = 2^31 - 1.
-  The computations here are done primitively, especially the implementation of inverse.
-  Later we could replace the implementations with mathcomp.
- *)
-Module Mersenne31 <: PrimeField.
-  Definition p : Z := 2147483647.  (* 2^31 - 1 *)
+(* Core module type with just the prime *)
+Module Type PrimeParameter.
+  Parameter p : Z.
+  Parameter p_prime : IsPrime p.
+End PrimeParameter.
 
-  (* Modulo utility for all prime fields. *)
+(* Functor that implements all field operations given a prime *)
+Module MakePrimeField (P : PrimeParameter) <: PrimeField.
+  Definition p := P.p.
   Definition mod_p (x : Z) : Z := Z.modulo x p.
-
-  (* Proof that the field is in the range [0, p-1] *)
-  (* This is a placeholder, we will need to prove this later. *)
-  (* We can use Z.modulo to ensure the result is in the range. *)
-  (* The proof obligation is that all operations are within the prime field. *)
   
-  (* Proof obligation: for all x, 0 <= x < p *)
-  Lemma mod_p_in_range : forall x, 0 <= Z.modulo x p < p.
-  Proof.
-  Admitted.
-  
-  (* Field operations, for now use pure integer zero and one,
-  as we are dealing with prime fields. *)
+  (* Field operations *)
   Definition zero : Z := 0.
   Definition one : Z := 1.
-  
-  Definition add (a b : Z) : Z := 
-    mod_p (a + b).
-  
-  Definition mul (a b : Z) : Z :=
-    mod_p (a * b).
-    
-  Definition neg (a : Z) : Z :=
-    mod_p (p - a).
-    
-  Definition sub (a b : Z) : Z :=
-    mod_p (a + p - b).
-    
+  Definition add (a b : Z) : Z := mod_p (a + b).
+  Definition mul (a b : Z) : Z := mod_p (a * b).
+  Definition neg (a : Z) : Z := mod_p (p - a).
+  Definition sub (a b : Z) : Z := mod_p (a + p - b).
   Definition inv (a : Z) : Z := mod_p (a ^ (p - 2)).
-  
-  (* Division is defined as multiplication by the inverse *)
-  (* This is a placeholder, we will need to prove this later. *)
-  (* The proof obligation is that all operations are within the prime field. *)
-    
-    
   Definition div (a b : Z) : Z := mul a (inv b).
   
-  (* Conversion functions *)
-  (* Definition of_Z (z : Z) : F := exist _ (mod_p z) (mod_p_in_range z).  (* Proof obligation *)
-  Definition to_Z (a : F) : Z := 
-    match a with
-    | exist _ va _ => va
-    end. *)
-  
+  (* Integer conversions *)
   Definition of_nat (n : nat) : Z := Z.of_nat n.
   Definition to_nat (a : Z) : nat := Z.to_nat a.
   Definition of_bool (b : bool) : Z := if b then one else zero.
-
-  Axiom p_prime : IsPrime p.
-  Axiom add_comm : forall a b, add a b = add b a.
-  Axiom add_assoc : forall a b c, add (add a b) c = add a (add b c).
-  Axiom mul_comm : forall a b, mul a b = mul b a.
-  Axiom mul_assoc : forall a b c, mul (mul a b) c = mul a (mul b c).
-  Axiom add_zero : forall a, add a zero = a.
-  Axiom mul_one : forall a, mul a one = a.
-  Axiom mul_zero : forall a, mul a zero = zero.
-  Axiom add_neg : forall a, add a (neg a) = zero.
-  Axiom sub_def : forall a b, sub a b = add a (neg b).
-  Axiom div_def : forall a b, div a b = mul a (inv b).
-  Axiom mul_inv : forall a, a <> zero -> mul a (inv a) = one.
-  Axiom distrib : forall a b c, mul a (add b c) = add (mul a b) (mul a c).
-
   
-  (* Boolean to field conversion *)
-  Axiom of_bool_true : of_bool true = one.
-  Axiom of_bool_false : of_bool false = zero.    
-End Mersenne31.
- *)
+  (* Use the provided prime proof *)
+  Definition p_prime := P.p_prime.
+  
+  (* Additional operations *)
+  Definition double (a : Z) : Z := add a a.
+  Definition square (a : Z) : Z := mul a a.
+  Definition cube (a : Z) : Z := mul (square a) a.
+  Definition xor (a b : Z) : Z := sub (add a b) (mul a (double b)).
+  Definition xor3 (a b c : Z) : Z := xor (xor a b) c.
+  Definition andn (a b : Z) : Z := mul (sub one a) b.
+  
+  (* Proofs of field properties *)
+  Lemma add_comm : forall a b, add a b = add b a.
+  Proof.
+    intros a b. unfold add, mod_p.
+    rewrite Z.add_comm. reflexivity.
+  Qed.
+  
+  Lemma add_assoc : forall a b c, add (add a b) c = add a (add b c).
+  Proof. Admitted.
+  
+  Lemma mul_comm : forall a b, mul a b = mul b a.
+  Proof. Admitted.
+  
+  Lemma mul_assoc : forall a b c, mul (mul a b) c = mul a (mul b c).
+  Proof. Admitted.
+  
+  Lemma add_zero : forall a, add a zero = a.
+  Proof. Admitted.
+  
+  Lemma mul_one : forall a, mul a one = a.
+  Proof. Admitted.
+  
+  Lemma mul_zero : forall a, mul a zero = zero.
+  Proof. Admitted.
+  
+  Lemma add_neg : forall a, add a (neg a) = zero.
+  Proof. Admitted.
+  
+  Lemma sub_def : forall a b, sub a b = add a (neg b).
+  Proof. Admitted.
+  
+  Lemma div_def : forall a b, div a b = mul a (inv b).
+  Proof. Admitted.
+  
+  Lemma mul_inv : forall a, a <> zero -> mul a (inv a) = one.
+  Proof. Admitted.
+  
+  Lemma distrib : forall a b c, mul a (add b c) = add (mul a b) (mul a c).
+  Proof. Admitted.
+  
+  Lemma of_bool_true : of_bool true = one.
+  Proof. unfold of_bool, one. reflexivity. Qed.
+  
+  Lemma of_bool_false : of_bool false = zero.
+  Proof. unfold of_bool, zero. reflexivity. Qed.
+End MakePrimeField.
+
+(* Define Mersenne31 prime as the prime parameter module *)
+Module Mersenne31Parameter <: PrimeParameter.
+  Definition p : Z := 2147483647.  (* 2^31 - 1 *)
+  
+  Lemma p_prime : IsPrime p.
+  Proof. Admitted.
+End Mersenne31Parameter.
+
+Module Mersenne31 := MakePrimeField(Mersenne31Parameter).
