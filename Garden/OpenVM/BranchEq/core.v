e9b500fddc65f9b8e21ec4c7ce69bf48755179bf
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2025-06-07 12:35:01 +0800

    update

--- Garden/OpenVM/BranchEq/core.v
+++ Garden/OpenVM/BranchEq/core.v
@@ -1,6 +1,5 @@
 (* 
 TODO:
-- VmAdapterInterface
 - InteractionBuilder
 *)
 
@@ -58,6 +57,47 @@ Module Dependency.
     bus_index : BusIndex;
     count_weight : nat;
   }.
+
+  (* 
+  /// The interface between primitive AIR and machine adapter AIR.
+  pub trait VmAdapterInterface<T> {
+      /// The memory read data that should be exposed for downstream use
+      type Reads;
+      /// The memory write data that are expected to be provided by the integrator
+      type Writes;
+      /// The parts of the instruction that should be exposed to the integrator.
+      /// This will typically include `is_valid`, which indicates whether the trace row
+      /// is being used and `opcode` to indicate which opcode is being executed if the
+      /// VmChip supports multiple opcodes.
+      type ProcessedInstruction;
+  }
+  *)
+  Class VmAdapterInterface (T : Set) : Set := {
+    Reads : Set;
+    Writes : Set;
+    ProcessedInstruction : Set;
+  }.
+
+  (* 
+  pub trait InteractionBuilder: AirBuilder {
+      /// Stores a new interaction in the builder.
+      ///
+      /// See [Interaction] for more details on `count_weight`.
+      fn push_interaction<E: Into<Self::Expr>>(
+          &mut self,
+          bus_index: BusIndex,
+          fields: impl IntoIterator<Item = E>,
+          count: impl Into<Self::Expr>,
+          count_weight: u32,
+      );
+  
+      /// Returns the current number of interactions.
+      fn num_interactions(&self) -> usize;
+  
+      /// Returns all interactions stored.
+      fn all_interactions(&self) -> &[Interaction<Self::Expr>];
+  }
+  *)
 End Dependency.
 
 (*
@@ -148,115 +188,114 @@ TODO:
 *)
 (* TODO: define custom type for I *)
 (* Types *)
- Context InteractionBuilder : Set.
- Context I : VmAdapterInterface.
- Context NUM_LIMBS : nat.
+  Context InteractionBuilder : Set.
+  Context I : VmAdapterInterface.
+  Context NUM_LIMBS : nat.
 
- Parameter AB : InteractionBuilder.
-
-(* 
-fn eval(
-        &self,
-        builder: &mut AB,
-        local: &[AB::Var],
-        from_pc: AB::Var,
-    ) -> AdapterAirContext<AB::Expr, I> {
-*)
-Definition eval (builder : AB) (local : list (AB.(Var))) (from_pc : AB.Var) : AdapterAirContext Expr I :=
-  (* 
-  let cols: &BranchEqualCoreCols<_, NUM_LIMBS> = local.borrow();
-  let flags = [cols.opcode_beq_flag, cols.opcode_bne_flag];
-  *)
-  let cols := local in
-  let flags := cols.(opcode_beq_flag) :: [cols.(opcode_bne_flag)] in
+  Parameter AB : InteractionBuilder.
 
   (* 
-  let is_valid = flags.iter().fold(AB::Expr::ZERO, |acc, &flag| {
-            builder.assert_bool(flag);
-            acc + flag.into()
-        });
+  fn eval(
+          &self,
+          builder: &mut AB,
+          local: &[AB::Var],
+          from_pc: AB::Var,
+      ) -> AdapterAirContext<AB::Expr, I> {
   *)
-  let is_valid := _ in
-  tt.
+  Definition eval (builder : AB) (local : list (AB.(Var))) (from_pc : AB.Var) : AdapterAirContext Expr I :=
+    (* 
+    let cols: &BranchEqualCoreCols<_, NUM_LIMBS> = local.borrow();
+    let flags = [cols.opcode_beq_flag, cols.opcode_bne_flag];
+    *)
+    let cols := local in
+    let flags := cols.(opcode_beq_flag) :: [cols.(opcode_bne_flag)] in
+
+    (* 
+    let is_valid = flags.iter().fold(AB::Expr::ZERO, |acc, &flag| {
+              builder.assert_bool(flag);
+              acc + flag.into()
+          });
+    *)
+    let is_valid := _ in
+    tt.
+
+    (* 
+    builder.assert_bool(is_valid.clone());
+    builder.assert_bool(cols.cmp_result);
+    *)
+    let assert_result := if _ then true else false in
+    let assert_result := if _ then assert_result else false in
+    (* TODO: completely ignore all the followings if assertion is false *)
+
+    (* 
+    let a = &cols.a;
+    let b = &cols.b;
+    let inv_marker = &cols.diff_inv_marker;
+    *)
+    let a := cols.(a) in
+    let b := cols.(b) in
+    let inv_maker := cols.(diff_inv_marker) in
+
+    (* 
+    // 1 if cmp_result indicates a and b are equal, 0 otherwise
+    let cmp_eq =
+        cols.cmp_result * cols.opcode_beq_flag + not(cols.cmp_result) * cols.opcode_bne_flag;
+    let mut sum = cmp_eq.clone();
+    *)
+    let cmp_eq := cols.cmp_result * cols.opcode_beq_flag 
+      + not(cols.cmp_result) * cols.opcode_bne_flag in
+    let sum := cmp_eq in
+
+
 
   (* 
-  builder.assert_bool(is_valid.clone());
-  builder.assert_bool(cols.cmp_result);
+          // For BEQ, inv_marker is used to check equality of a and b:
+          // - If a == b, all inv_marker values must be 0 (sum = 0)
+          // - If a != b, inv_marker contains 0s for all positions except ONE position i where a[i] !=
+          //   b[i]
+          // - At this position, inv_marker[i] contains the multiplicative inverse of (a[i] - b[i])
+          // - This ensures inv_marker[i] * (a[i] - b[i]) = 1, making the sum = 1
+          // Note: There might be multiple valid inv_marker if a != b.
+          // But as long as the trace can provide at least one, that’s sufficient to prove a != b.
+          //
+          // Note:
+          // - If cmp_eq == 0, then it is impossible to have sum != 0 if a == b.
+          // - If cmp_eq == 1, then it is impossible for a[i] - b[i] == 0 to pass for all i if a != b.
+          for i in 0..NUM_LIMBS {
+              sum += (a[i] - b[i]) * inv_marker[i];
+              builder.assert_zero(cmp_eq.clone() * (a[i] - b[i]));
+          }
+          builder.when(is_valid.clone()).assert_one(sum);
+
+          let expected_opcode = flags
+              .iter()
+              .zip(BranchEqualOpcode::iter())
+              .fold(AB::Expr::ZERO, |acc, (flag, opcode)| {
+                  acc + ( \*flag).into() * AB::Expr::from_canonical_u8(opcode as u8)
+              })
+              + AB::Expr::from_canonical_usize(self.offset);
+
+          let to_pc = from_pc
+              + cols.cmp_result * cols.imm
+              + not(cols.cmp_result) * AB::Expr::from_canonical_u32(self.pc_step);
+
+          AdapterAirContext {
+              to_pc: Some(to_pc),
+              reads: [cols.a.map(Into::into), cols.b.map(Into::into)].into(),
+              writes: Default::default(),
+              instruction: ImmInstruction {
+                  is_valid,
+                  opcode: expected_opcode,
+                  immediate: cols.imm.into(),
+              }
+              .into(),
+          }
+      }
   *)
-  let assert_result := if _ then true else false in
-  let assert_result := if _ then assert_result else false in
-  (* TODO: completely ignore all the followings if assertion is false *)
-
-  (* 
-  let a = &cols.a;
-  let b = &cols.b;
-  let inv_marker = &cols.diff_inv_marker;
-  *)
-  let a := cols.(a) in
-  let b := cols.(b) in
-  let inv_maker := cols.(diff_inv_marker) in
-
-  (* 
-  // 1 if cmp_result indicates a and b are equal, 0 otherwise
-  let cmp_eq =
-      cols.cmp_result * cols.opcode_beq_flag + not(cols.cmp_result) * cols.opcode_bne_flag;
-  let mut sum = cmp_eq.clone();
-  *)
-  let cmp_eq := cols.cmp_result * cols.opcode_beq_flag 
-    + not(cols.cmp_result) * cols.opcode_bne_flag in
-  let sum := cmp_eq in
-
-
-
-(* 
-        // For BEQ, inv_marker is used to check equality of a and b:
-        // - If a == b, all inv_marker values must be 0 (sum = 0)
-        // - If a != b, inv_marker contains 0s for all positions except ONE position i where a[i] !=
-        //   b[i]
-        // - At this position, inv_marker[i] contains the multiplicative inverse of (a[i] - b[i])
-        // - This ensures inv_marker[i] * (a[i] - b[i]) = 1, making the sum = 1
-        // Note: There might be multiple valid inv_marker if a != b.
-        // But as long as the trace can provide at least one, that’s sufficient to prove a != b.
-        //
-        // Note:
-        // - If cmp_eq == 0, then it is impossible to have sum != 0 if a == b.
-        // - If cmp_eq == 1, then it is impossible for a[i] - b[i] == 0 to pass for all i if a != b.
-        for i in 0..NUM_LIMBS {
-            sum += (a[i] - b[i]) * inv_marker[i];
-            builder.assert_zero(cmp_eq.clone() * (a[i] - b[i]));
-        }
-        builder.when(is_valid.clone()).assert_one(sum);
-
-        let expected_opcode = flags
-            .iter()
-            .zip(BranchEqualOpcode::iter())
-            .fold(AB::Expr::ZERO, |acc, (flag, opcode)| {
-                acc + ( \*flag).into() * AB::Expr::from_canonical_u8(opcode as u8)
-            })
-            + AB::Expr::from_canonical_usize(self.offset);
-
-        let to_pc = from_pc
-            + cols.cmp_result * cols.imm
-            + not(cols.cmp_result) * AB::Expr::from_canonical_u32(self.pc_step);
-
-        AdapterAirContext {
-            to_pc: Some(to_pc),
-            reads: [cols.a.map(Into::into), cols.b.map(Into::into)].into(),
-            writes: Default::default(),
-            instruction: ImmInstruction {
-                is_valid,
-                opcode: expected_opcode,
-                immediate: cols.imm.into(),
-            }
-            .into(),
-        }
-    }
-*)
 
 End Impl_VmCoreAir_for_BranchEqualCoreAir.
-(*
-    
 
+(*
     fn start_offset(&self) -> usize {
         self.offset
     }
