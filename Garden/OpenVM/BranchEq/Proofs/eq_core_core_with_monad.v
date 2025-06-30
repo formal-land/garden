70dc9cd2fcb175bcabeb6b37cf069a3ac644c94b
InfiniteEchoes <128528395+InfiniteEchoes@users.noreply.github.com>
2025-06-30 23:37:27 +0800

    minor: progress on proving equality

--- Garden/OpenVM/BranchEq/Proofs/eq_core_core_with_monad.v
+++ Garden/OpenVM/BranchEq/Proofs/eq_core_core_with_monad.v
@@ -19,6 +19,9 @@ Module MBuilder_test.
     b1 = b2. Admitted.
   Definition test2 (x : Z) := let*B _ := assert_zero x in M.Pure tt.
   Definition test3 (x : Z) := let*B _ := assert_bool x in M.Pure tt.
+  Definition test4 := 
+    let f := fun (x : Z) => (x + x) in
+    [[ f 4 ]]*.
 End MBuilder_test.
 
 Module MLessEffects_test.
@@ -27,11 +30,11 @@ Module MLessEffects_test.
   Definition test1 := let* _ := M.Pure tt in M.Pure tt.
   Definition test2 (x : Z) := let* _ := assert_zero x in M.Pure tt.
   Definition test3 {p} `{Prime p} (x : Z) := let* _ := assert_bool x in M.Pure tt.
+  Definition test4 := 
+    let f := fun (x : Z) => (x + x) in
+    [[ f 4 ]].
 End MLessEffects_test.
 
-(* TODO:
-- How to show that 2 programs generate the same constraints?
-*)
 Module Equality.
   Import Garden.OpenVM.BranchEq.M.MBuilder.
   Import Garden.Plonky3.MLessEffects. 
@@ -42,18 +45,47 @@ Module Equality.
   Definition test1_notation := forall (o1 o2 : unit) (b1 b2 : Builder.t),
   {{ [[ MBuilder_test.test1 ]]* | b1 🔽 o1 | b2 }}*.
 
+  (* Required for dependent destruction *)
+  Require Import Coq.Program.Equality.
+
   Theorem eq_output_test1 : forall (o1 o2 : unit) (b1 b2 : Builder.t),
     {{ [[ MBuilder_test.test1 ]]* | b1 🔽 o1 | b2 }}* ->
     {{ [[ MLessEffects_test.test1 ]] 🔽 o2, True }} ->
     o1 = o2.
   Admitted.
+  
+  (* 
+  Theorem eq_output_test2 : forall (x1 x2 : Z) (o1 o2 : unit),
+    x1 = x2 ->
+    {{ MBuilder_test.test2 x1 | b 🔽 o1 | b }} ->
+    {{ MLessEffects_test.test2 x2 🔽 o2 , True }} ->
+    o1 = o2.
+  *)
 
-
-(* 
-Theorem eq_output_test2 : forall (x1 x2 : Z) (o1 o2 : unit),
-  x1 = x2 ->
-  {{ MBuilder_test.test2 x1 | b 🔽 o1 | b }} ->
-  {{ MLessEffects_test.test2 x2 🔽 o2 , True }} ->
+  Theorem eq_test4 : forall (o1 o2 : Z) (b1 b2 : Builder.t),
+  {{ [[ MBuilder_test.test4 ]]* | b1 🔽 o1 | b2 }}* ->
+  {{ [[ MLessEffects_test.test4 ]] 🔽 o2, True }} ->
   o1 = o2.
-*)
+  Proof.
+    intros o1 o2 b1 b2 runB4 runLE4.
+    cbv in runB4, runLE4.
+    (* Prove o1 = 8 *)
+    inversion runB4.
+    dependent destruction H1.
+    dependent destruction H2.
+    (* TODO: Prove o2 = 8 *)
+    inversion runLE4.
+    dependent destruction H1.
+    dependent destruction H3.
+    (* Stuck *)
+    injection x.
+    Abort.
+    (* subst in H1. *)
+    (* inversion runLE4. *)
+    (* rewrite <- eqbuilder in runB4. *)
+
+    (* TODO: prove o1 = 8 from Run.Pure's formation *)
+    (* apply (MBuilder.Run.Pure b1 8) in runB4. *)
+  (* Admitted. *)
+
 End Equality.
