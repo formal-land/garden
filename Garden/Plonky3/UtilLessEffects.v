bc6069c805e1778b450688229c12b6a476e38c24
Yunkai Zhang <yunkai.zhang22@imperial.ac.uk>
2025-07-21 14:25:17 +0100

    feat: updated new add2 and add3 gates

--- Garden/Plonky3/UtilLessEffects.v
+++ Garden/Plonky3/UtilLessEffects.v
@@ -108,3 +108,166 @@ Definition xor_32_shift {p} `{Prime p}
   let* zero_1 := assert_zero (BinOp.sub (Array.get a 0) sum_0_16) in
   let* zero_2 := assert_zero (BinOp.sub (Array.get a 1) sum_16_32) in
   M.Pure tt.
+
+
+(*
+#[inline]
+pub fn add2<AB: AirBuilder>(
+    builder: &mut AB,
+    a: &[AB::Var; 2],
+    b: &[AB::Var; 2],
+    c: &[AB::Expr; 2],
+) {
+    // Define:
+    //  acc    = a - b - c (mod P)
+    //  acc_16 = a[0] - b[0] - c[0] (mod P)
+    //
+    // We perform 2 checks:
+    //
+    // (1) acc*(acc + 2^32) = 0.
+    // (2) acc_16*(acc_16 + 2^16) = 0.
+    //
+    // We give a short proof for why this lets us conclude that a = b + c mod 2^32:
+    //
+    // As all 16 bit limbs have been range checked, we know that a, b, c lie in [0, 2^32) and hence
+    // a = b + c mod 2^32 if and only if, over the integers, a - b - c = 0 or -2^32.
+    //
+    // Equation (1) verifies that either a - b - c = 0 mod P or a - b - c = -2^32 mod P.
+    //
+    // Field overflow cannot occur when computing acc_16 as our characteristic is larger than 2^17.
+    // Hence, equation (2) verifies that, over the integers, a[0] - b[0] - c[0] = 0 or -2^16.
+    // Either way we can immediately conclude that a - b - c = 0 mod 2^16.
+    //
+    // Now we can use the chinese remainder theorem to combine these results to conclude that
+    // either a - b - c = 0 mod 2^16 P or a - b - c = -2^32 mod 2^16 P.
+    //
+    // No overflow can occur mod 2^16 P as 2^16 P > 2^33 and a, b, c < 2^32. Hence we conclude that
+    // over the integers a - b - c = 0 or a - b - c = -2^32 which is equivalent to a = b + c mod 2^32.
+
+    // By assumption P > 2^17 so 1 << 16 will be less than P. We use the checked version just to be safe.
+    // The compiler should optimize it away.
+    let two_16 =
+        <AB::Expr as PrimeCharacteristicRing>::PrimeSubfield::from_canonical_checked(1 << 16)
+            .unwrap();
+    let two_32 = two_16.square();
+
+    let acc_16 = a[0] - b[0] - c[0].clone();
+    let acc_32 = a[1] - b[1] - c[1].clone();
+    let acc = acc_16.clone() + acc_32.mul_2exp_u64(16);
+
+    builder.assert_zeros([
+        acc.clone() * (acc + AB::Expr::from_prime_subfield(two_32)),
+        acc_16.clone() * (acc_16 + AB::Expr::from_prime_subfield(two_16)),
+    ]);
+}
+*)
+Definition add2 {p} `{Prime p} 
+    (a : Array.t Z 2)      (* Result array with 2 limbs *)
+    (b : Array.t Z 2)      (* First input array with 2 limbs *)
+    (c : Array.t Z 2)      (* Second input array with 2 limbs *)
+    : M.t unit :=
+    (*
+    let two_16 =
+        <AB::Expr as PrimeCharacteristicRing>::PrimeSubfield::from_canonical_checked(1 << 16)
+            .unwrap();    
+    *)
+    let two_16 := UnOp.from (2 ^ 16) in
+    (* let two_32 = two_16.square(); *)
+    let two_32 := BinOp.mul two_16 two_16 in
+    (* let acc_16 = a[0] - b[0] - c[0].clone(); *)
+    let acc_16 := BinOp.sub (BinOp.sub (Array.get a 0) (Array.get b 0)) (Array.get c 0) in
+    (* let acc_32 = a[1] - b[1] - c[1].clone(); *)
+    let acc_32 := BinOp.sub (BinOp.sub (Array.get a 1) (Array.get b 1)) (Array.get c 1) in
+    (* let acc = acc_16.clone() + acc_32.mul_2exp_u64(16); *)
+    let acc := BinOp.add acc_16 (BinOp.mul acc_32 (2 ^ 16)) in
+    (*     
+    builder.assert_zeros([
+        acc.clone() * (acc + AB::Expr::from_prime_subfield(two_32)),
+        acc_16.clone() * (acc_16 + AB::Expr::from_prime_subfield(two_16)),
+    ]); 
+    *)
+    let* _ := assert_zero (BinOp.mul acc (BinOp.add acc two_32)) in
+    let* _ := assert_zero (BinOp.mul acc_16 (BinOp.add acc_16 two_16)) in
+    M.Pure tt.
+
+(*
+#[inline]
+pub fn add3<AB: AirBuilder>(
+    builder: &mut AB,
+    a: &[AB::Var; 2],
+    b: &[AB::Var; 2],
+    c: &[AB::Expr; 2],
+    d: &[AB::Expr; 2],
+) {
+    // Define:
+    //  acc    = a - b - c - d (mod P)
+    //  acc_16 = a[0] - b[0] - c[0] - d[0] (mod P)
+    //
+    // We perform 2 checks:
+    //
+    // (1) acc*(acc + 2^32)*(acc + 2*2^32) = 0.
+    // (2) acc_16*(acc_16 + 2^16)*(acc_16 + 2*2^16) = 0.
+
+    // No overflow can occur mod 2^16 P as 2^16 P > 3*2^32 and a, b, c, d < 2^32. Hence we conclude that
+    // over the integers a - b - c - d = 0, -2^32 or -2*2^32 which implies a = b + c + d mod 2^32.
+
+    // By assumption P > 3*2^16 so 1 << 16 will be less than P. We use the checked version just to be safe.
+    // The compiler should optimize it away.
+    let two_16 =
+        <AB::Expr as PrimeCharacteristicRing>::PrimeSubfield::from_canonical_checked(1 << 16)
+            .unwrap();
+    let two_32 = two_16.square();
+
+    let acc_16 = a[0] - b[0] - c[0].clone() - d[0].clone();
+    let acc_32 = a[1] - b[1] - c[1].clone() - d[1].clone();
+    let acc = acc_16.clone() + acc_32.mul_2exp_u64(16);
+
+    builder.assert_zeros([
+        acc.clone()
+            * (acc.clone() + AB::Expr::from_prime_subfield(two_32))
+            * (acc + AB::Expr::from_prime_subfield(two_32.double())),
+        acc_16.clone()
+            * (acc_16.clone() + AB::Expr::from_prime_subfield(two_16))
+            * (acc_16 + AB::Expr::from_prime_subfield(two_16.double())),
+    ]);
+}
+*)
+Definition add3 {p} `{Prime p} 
+    (a : Array.t Z 2)      (* Result array with 2 limbs *)
+    (b : Array.t Z 2)      (* First input array with 2 limbs *)
+    (c : Array.t Z 2)      (* Second input array with 2 limbs *)
+    (d : Array.t Z 2)      (* Third input array with 2 limbs *)
+    : M.t unit :=
+    (*
+    let two_16 =
+        <AB::Expr as PrimeCharacteristicRing>::PrimeSubfield::from_canonical_checked(1 << 16)
+            .unwrap();    
+    *)
+    let two_16 := UnOp.from (2 ^ 16) in
+    (* let two_32 = two_16.square(); *)
+    let two_32 := BinOp.mul two_16 two_16 in
+    (* let acc_16 = a[0] - b[0] - c[0].clone() - d[0].clone(); *)
+    let acc_16 := BinOp.sub (BinOp.sub (BinOp.sub (Array.get a 0) (Array.get b 0)) (Array.get c 0)) (Array.get d 0) in
+    (* let acc_32 = a[1] - b[1] - c[1].clone() - d[1].clone(); *)
+    let acc_32 := BinOp.sub (BinOp.sub (BinOp.sub (Array.get a 1) (Array.get b 1)) (Array.get c 1)) (Array.get d 1) in
+    (* let acc = acc_16.clone() + acc_32.mul_2exp_u64(16); *)
+    let acc := BinOp.add acc_16 (BinOp.mul acc_32 (2 ^ 16)) in
+    (*     
+    builder.assert_zeros([
+        acc.clone()
+            * (acc.clone() + AB::Expr::from_prime_subfield(two_32))
+            * (acc + AB::Expr::from_prime_subfield(two_32.double())),
+        acc_16.clone()
+            * (acc_16.clone() + AB::Expr::from_prime_subfield(two_16))
+            * (acc_16 + AB::Expr::from_prime_subfield(two_16.double())),
+    ]);
+    *)
+    let* _ := assert_zero 
+      (BinOp.mul 
+        (BinOp.mul acc (BinOp.add acc two_32))
+        (BinOp.add acc (double two_32))) in
+    let* _ := assert_zero 
+      (BinOp.mul 
+        (BinOp.mul acc_16 (BinOp.add acc_16 two_16))
+        (BinOp.add acc_16 (double two_16))) in
+    M.Pure tt.
