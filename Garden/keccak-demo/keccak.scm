29ac9b5f7e8addb98df4cfa8fb42d97c73f5cc93
sanjay <60250789+tfmsadhith@users.noreply.github.com>
2025-08-06 20:24:00 +0800

    Page 5 to 6 of the spec.

--- Garden/keccak-demo/keccak.scm
+++ Garden/keccak-demo/keccak.scm
@@ -1,5 +1,5 @@
-;; Sanjay Adhith
-;; Time-stamp: <2025-07-28 23:01:21 adhithsanjay>
+;; Formal Land
+;; Time-stamp: <2025-08-06 20:20:31 adhithsanjay>
 
 ;; This is an implementation of the Keccak algorithm
 ;; as specified in:
@@ -26,8 +26,8 @@
 (unless (test-bit-xor bit-xor)
   (errorf 'bit-xor "bit-xor has an error"))
 
-;; "For a positive integer s, 0^s is the string that consists of s consecutive 0s.
-;; If s=0, then 0^s is the empty string."
+;; For a positive integer s, 0^s is the string that consists of s consecutive 0s.
+;; If s=0, then 0^s is the empty string.
 
 (define (zero-string s)
   (make-vector s 0))
@@ -35,12 +35,15 @@
 (unless (test-zero-string zero-string)
   (errorf 'zero-string "zero-string has an error"))
 
-;; "For a bit string X, len(X) is the length of X in bits."
+;; For a bit string X, len(X) is the length of X in bits.
 
-(define (length x)
+(define (len x)
   (if (not (vector? x))
       (errorf 'x "not a vector x")
       (vector-length x)))
+
+(unless (test-len len)
+  (errorf 'len "len has an error"))
 
 ;; For a string X and an integer i such that 0 ≤ i < len(X), X[i] is the bit of X
 ;; with index i.
@@ -54,7 +57,7 @@
 	 (errorf 'x "not a vector x")]
 	[(not (integer? i))
 	 (errorf 'i "not an integer i")]
-	[(not (and (<= 0 i) (< i (length x))))
+	[(not (and (<= 0 i) (< i (len x))))
 	 (errorf 'i "out of bounds i")]
 	[else 
 	 (vector-ref x i)]))
@@ -72,7 +75,7 @@
 	 (errorf 'x "not a vector x")]
 	[(not (integer? s))
 	 (errorf 's "not an integer s")]
-	[(not (and (< 0 s) (< s (length x))))
+	[(not (and (< 0 s) (< s (len x))))
 	 (errorf 's "out of bounds s")]
 	[else 
 	 (vector-copy x 0 s)]))
@@ -80,3 +83,73 @@
 (unless (test-trunc trunc)
   (errorf 'trunc "trunc has an error"))
 
+;; For strings X and Y of equal bit length, X XOR Y
+;; is the string that results from applying the Boolean
+;; exclusive-OR operation to X and Y at each bit position.
+;; For example, 1100 XOR 1010 = 0110.
+
+(define (string-XOR x y)
+  (cond [(not (vector? x))
+	 (errorf 'x "not a vector x")]
+	[(not (vector? y))
+	 (errorf 'y "not a vector y")]
+	[(not (= (len x) (len y)))
+	 (errorf '"x and y" "not strings of equal length")]
+	[(vector-map bit-xor x y)]))
+
+(unless (test-string-XOR string-XOR)
+  (errorf 'string-XOR "string-XOR has an error"))
+
+;; For strings X and Y, X || Y is the concatenation of X and Y.
+;; For example, 11001 || 010 = 11001010.
+
+(define (string-concat x y)
+  (cond [(not (vector? x))
+	 (errorf 'x "not a vector x")]
+	[(not (vector? y))
+	 (errorf 'y "not a vector y")]
+	[(list->vector
+	  (append (vector->list x) (vector->list y)))]))
+
+(unless (test-string-concat string-concat)
+  (errorf 'string-concat "string-concat has an error"))
+
+;; For integers m and n, m/n is the quotient, i.e., m divided by n.
+
+;; Here we use the quotient function in Scheme.
+;; Example:
+;; (quotient 15 4)
+;; => 3
+
+;; For integers m and n, m mod n is the integer r for which 0 ≤ r < n
+;; and m - r is a multiple of n. For example, 11 mod 5 = 1, and
+;; -11 mod 5 = 4.
+
+;; Here we use the mod function in Scheme.
+;; Example:
+;; (mod -11 5)
+;; => 4
+
+;; For a real number x, ceil(x) is the least integer that is not
+;; strictly less than x.
+
+;; Here we use the ceiling function in Scheme.
+;; Example:
+;; (ceiling -3.3)
+;; => -3.0
+
+;; For a positive real number x, log_2(x) is the real number y such
+;; that 2^y = x.
+
+(define (log_2 x)
+  (cond [(not (and (number? x) (> 0)))
+	 (errorf 'x "not a positive number x")]
+	[(/ (log x) (log 2))]))
+
+;; For real numbers x and y, min(x, y) is the minimum of x and y. For
+;; example, min(9, 33) = 9.
+
+;; Here we use the min function in Scheme.
+;; Example:
+;; (min 9 33)
+;; => 9
--- Garden/keccak-demo/tests.scm
+++ Garden/keccak-demo/tests.scm
@@ -1,5 +1,5 @@
 ;; Sanjay Adhith
-;; Time-stamp: <2025-07-29 02:23:27 adhithsanjay>
+;; Time-stamp: <2025-08-06 20:22:51 adhithsanjay>
 
 ;; Tests for keccak.scm
 
@@ -62,6 +62,20 @@
 	     [expected-result (make-vector 10 0)])
 	 (equal? actual-result expected-result))))
 
+(define (test-len candidate)
+  (and (let ([actual-result (candidate '#(1 0 1 0 0 0))]
+	     [expected-result 6])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#(0 0 0 0))]
+	     [expected-result 4])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#(0))]
+	     [expected-result 1])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#())]
+	     [expected-result 0])
+	 (equal? actual-result expected-result))))
+
 (define (test-ith candidate)
   (and (let ([actual-result (candidate '#(1 0 1 0 0 0) 2)]
 	     [expected-result 1])
@@ -83,3 +97,76 @@
        (let ([actual-result (candidate 3 '#(1 1 1 1 0))]
 	     [expected-result '#(1 1 1)])
 	 (equal? actual-result expected-result))))
+
+(define (test-string-XOR candidate)
+  (and (let ([actual-result (candidate '#(0) '#(0))]
+	     [expected-result '#(0)])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#(1) '#(1))]
+	     [expected-result '#(0)])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#(1) '#(0))]
+	     [expected-result '#(1)])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#(0) '#(1))]
+	     [expected-result '#(1)])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#(1 1 0 0) '#(1 0 1 0))]
+	     [expected-result '#(0 1 1 0)])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#(1 1 0 0) '#(1 0 1 1))]
+	     [expected-result '#(0 1 1 1)])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#(1 1 1 1) '#(1 0 1 0))]
+	     [expected-result '#(0 1 0 1)])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#(0 0 0 0) '#(1 0 1 0))]
+	     [expected-result '#(1 0 1 0)])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#(0 0 0 0 0) '#(1 0 1 0 0))]
+	     [expected-result '#(1 0 1 0 0)])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#(0 0 0 0 0) '#(1 0 1 0 1))]
+	     [expected-result '#(1 0 1 0 1)])
+	 (equal? actual-result expected-result))
+       ))
+
+(define (test-string-concat candidate)
+  (and (let ([actual-result (candidate '#(0) '#(0))]
+	     [expected-result '#(0 0)])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#(1) '#(1))]
+	     [expected-result '#(1 1)])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#(1) '#(0))]
+	     [expected-result '#(1 0)])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#(0) '#(1))]
+	     [expected-result '#(0 1)])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#(1 1 0 0) '#(1 0 1 0))]
+	     [expected-result '#(1 1 0 0 1 0 1 0)])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#(1 1 0 0) '#(1 0 1 1))]
+	     [expected-result '#(1 1 0 0 1 0 1 1)])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#(1 1 1 1) '#(1 0 1 0))]
+	     [expected-result '#(1 1 1 1 1 0 1 0)])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#(0 0 0 0) '#(1 0 1 0))]
+	     [expected-result '#(0 0 0 0 1 0 1 0)])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#(0 0 0 0 0) '#(1 0 1 0 0))]
+	     [expected-result '#(0 0 0 0 0 1 0 1 0 0)])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate '#(0 0 0 0 0) '#(1 0 1 0 1))]
+	     [expected-result '#(0 0 0 0 0 1 0 1 0 1)])
+	 (equal? actual-result expected-result))))
+
+(define (test-log_2 candidate)
+  (and (let ([actual-result (candidate 32)]
+	     [expected-result 5.0])
+	 (equal? actual-result expected-result))
+       (let ([actual-result (candidate 64)]
+	     [expected-result 6.0])
+         (equal? actual-result expected-result))))
